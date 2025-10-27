80776d698a4a6795ec5e03e94803dc8e41e7941b
Parham Alvani <parham.alvani@gmail.com>
2025-10-27 11:21:53 +0000

    chore: disable third party tests to see how it improves time

--- tests/run.sh
+++ tests/run.sh
@@ -9,73 +9,73 @@ export TEST_CHEAT_PATH="${NAVI_HOME}/tests/no_prompt_cheats"
 export NAVI_EXE="${NAVI_HOME}/target/debug/navi"
 
 if ! command_exists navi; then
-  navi() {
-    "$NAVI_EXE" "$@"
-  }
-  export -f navi
+	navi() {
+		"$NAVI_EXE" "$@"
+	}
+	export -f navi
 fi
 
 _navi() {
-  stty sane || true
-  local path="${NAVI_TEST_PATH:-$TEST_CHEAT_PATH}"
-  path="${path//$HOME/~}"
-  export NAVI_ENV_VAR_PATH="$path"
-  RUST_BACKTRACE=1 "$NAVI_EXE" --path "$NAVI_ENV_VAR_PATH" "$@"
+	stty sane || true
+	local path="${NAVI_TEST_PATH:-$TEST_CHEAT_PATH}"
+	path="${path//$HOME/~}"
+	export NAVI_ENV_VAR_PATH="$path"
+	RUST_BACKTRACE=1 "$NAVI_EXE" --path "$NAVI_ENV_VAR_PATH" "$@"
 }
 
 _navi_cases() {
-  local -r filter="${1::-2}"
-  _navi --query "$filter" --best-match
+	local -r filter="${1::-2}"
+	_navi --query "$filter" --best-match
 }
 
 _navi_cases_test() {
-  _navi_cases "$1" |
-    test::equals "$2"
+	_navi_cases "$1" |
+		test::equals "$2"
 }
 
 _get_all_tests() {
-  grep '^#' "${TEST_CHEAT_PATH}/cases.cheat" |
-    grep ' ->' |
-    sed 's/\\n/'"$(printf "$NEWLINE_CHAR")"'/g' |
-    sed -E 's/# (.*) -> "(.*)"/\1|\2/g'
+	grep '^#' "${TEST_CHEAT_PATH}/cases.cheat" |
+		grep ' ->' |
+		sed 's/\\n/'"$(printf "$NEWLINE_CHAR")"'/g' |
+		sed -E 's/# (.*) -> "(.*)"/\1|\2/g'
 }
 
 _get_tests() {
-  local -r filter="$1"
+	local -r filter="$1"
 
-  if [ -n "$filter" ]; then
-    _get_all_tests |
-      grep "$filter"
-  else
-    _get_all_tests
-  fi
+	if [ -n "$filter" ]; then
+		_get_all_tests |
+			grep "$filter"
+	else
+		_get_all_tests
+	fi
 }
 
 _navi_tldr() {
-  _navi --tldr docker --query ps --print --best-match |
-    test::contains "docker ps"
+	_navi --tldr docker --query ps --print --best-match |
+		test::contains "docker ps"
 }
 
 _navi_cheatsh() {
-  _navi --cheatsh docker --query remove --print --best-match |
-    test::contains "docker container prune"
+	_navi --cheatsh docker --query remove --print --best-match |
+		test::contains "docker container prune"
 }
 
 _navi_widget() {
-  local -r out="$(_navi widget "$1")"
-  if ! echo "$out" | grep -q "navi "; then
-    echo "$out"
-    return 1
-  fi
+	local -r out="$(_navi widget "$1")"
+	if ! echo "$out" | grep -q "navi "; then
+		echo "$out"
+		return 1
+	fi
 }
 
 _navi_cheatspath() {
-  _navi info cheats-path |
-    grep -q "/cheats"
+	_navi info cheats-path |
+		grep -q "/cheats"
 }
 
 if ! command_exists fzf; then
-  export PATH="$PATH:$HOME/.fzf/bin"
+	export PATH="$PATH:$HOME/.fzf/bin"
 fi
 
 cd "$NAVI_HOME"
@@ -84,19 +84,19 @@ filter="${1:-}"
 
 # TODO: remove this
 if [[ $filter == "_navi" ]]; then
-  shift
-  _navi "$@"
-  exit 0
+	shift
+	_navi "$@"
+	exit 0
 fi
 
 test::set_suite "cases"
 ifs="$IFS"
 IFS=$'\n'
 for i in $(_get_tests "$filter"); do
-  IFS="$ifs"
-  query="$(echo "$i" | cut -d'|' -f1)"
-  expected="$(echo "$i" | tr "$NEWLINE_CHAR" '\n' | cut -d'|' -f2)"
-  test::run "$query" _navi_cases_test "$query" "$expected"
+	IFS="$ifs"
+	query="$(echo "$i" | cut -d'|' -f1)"
+	expected="$(echo "$i" | tr "$NEWLINE_CHAR" '\n' | cut -d'|' -f2)"
+	test::run "$query" _navi_cases_test "$query" "$expected"
 done
 
 test::set_suite "info"
@@ -109,8 +109,8 @@ test::run "fish" _navi_widget "fish"
 test::run "elvish" _navi_widget "elvish"
 test::run "nu" _navi_widget "nushell"
 
-test::set_suite "3rd party"
-test::run "tldr" _navi_tldr
-test::run "cheatsh" _navi_cheatsh
+# test::set_suite "3rd party"
+# test::run "tldr" _navi_tldr
+# test::run "cheatsh" _navi_cheatsh
 
 test::finish
